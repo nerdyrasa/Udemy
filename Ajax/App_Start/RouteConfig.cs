aedbe1515e92264de0db58fdf86038fccca2513d
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-08-17 17:36:58 -0500

    Enable attribute routing.

--- Ajax/App_Start/RouteConfig.cs
+++ Ajax/App_Start/RouteConfig.cs
@@ -13,6 +13,8 @@ namespace Ajax
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
