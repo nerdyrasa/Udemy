27bdfc86fe3951b50186b1b40ab21062e5da33e4
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-08-17 14:13:18 -0500

    Modify layout.

--- Ajax/App_Start/BundleConfig.cs
+++ Ajax/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Ajax
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-journal.css",
                       "~/Content/site.css"));
         }
     }
